d62206467bdb4b38b60cc469db6063115ce82226
pettittmj <pettittmj@gmail.com>
2016-11-15 10:11:09 -0500

    checkpoint 26 completed

--- scripts/album.js
+++ scripts/album.js
@@ -29,26 +29,10 @@
          { title: 'Wrong phone number', duration: '2:15'}
      ]
  };
-
-var albumPettitt = {
-     title: 'The Breaks',
-     artist: 'Matt Pettitt',
-     label: 'Streets 101',
-     year: '2003',
-     albumArtUrl: 'assets/images/album_covers/10.png',
-     songs: [
-         { title: 'Hard knock life', duration: '3:11' },
-         { title: 'Get what you work for', duration: '5:31' },
-         { title: 'Fall down, get up', duration: '1:30'},
-         { title: 'Fast times', duration: '3:14' },
-         { title: 'In the end', duration: '3:55'}
-     ]
- };
-
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -56,14 +40,15 @@ var createSongRow = function(songNumber, songName, songLength) {
  
      return template;
  };
-// Select elements that we want to populate with txt dynamically
- var albumTitle = document.getElementsByClassName('album-view-title')[0];
- var albumArtist = document.getElementsByClassName('album-view-artist')[0];
- var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
- var albumImage = document.getElementsByClassName('album-cover-art')[0];
- var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-var setCurrentAlbum = function(album) { 
+var setCurrentAlbum = function(album) {
+     // #1
+     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+     var albumImage = document.getElementsByClassName('album-cover-art')[0];
+     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ 
      // #2
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
@@ -78,16 +63,23 @@ var setCurrentAlbum = function(album) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
+ var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
+ window.onload = function() {
+     setCurrentAlbum(albumPicasso);
  
-window.onload = function() {
-    setCurrentAlbum(albumPicasso);
-    var albumList = [albumPicasso,albumMarconi,albumPettitt];
-    var index = 1;
-
-    var cover = document.getElementsByClassName('album-cover-art')[0];
-
-    cover.addEventListener("click", function() {
-        setCurrentAlbum(albumList[index]);
-        index = (index + 1) % (albumList.length);
-    });
-};
+     songListContainer.addEventListener('mouseover', function(event) {
+     if (event.target.parentElement.className === 'album-view-song-item') {
+          event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+     for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+         this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+ };
